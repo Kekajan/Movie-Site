e47140a4e05c23f24f02d093e2dede27ed28eebe
Kekajan <kekajanpk4220@gmail.com>
2023-08-10 16:47:26 +0530

    index updated

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-
   <?php include 'navbar.php'; ?>
 
   <!--carousel slide bars with images-->
@@ -64,127 +63,152 @@
   </div>
 
   <!-- Card -->
-  <section class="campus pb-5">
+  <div class="container">
     <h2 class="pt-5 pb-4 fw-bold text-start">Now Showing</h2>
-
-    <div class="row">
-      <div class="campus-col">
-        <img src="https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg">
-        <div class="layer">
-          <h3>Little Mermaid</h3>
-        </div>
-      </div>
-      <div class="campus-col">
-        <img src="https://m.media-amazon.com/images/M/MV5BZjYxYWVjMDMtZGRjZS00ZDE4LTk0OWUtMjUyOTI4MmYxNjgwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SL1024_.jpg">
-        <div class="layer">
-          <h3>Elemental</h3>
-        </div>
-      </div>
-      <div class="campus-col">
-        <img src="https://m.media-amazon.com/images/M/MV5BZTNiNDA4NmMtNTExNi00YmViLWJkMDAtMDAxNmRjY2I2NDVjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg">
-        <div class="layer">
-          <h3>Transformers</h3>
-        </div>
-      </div>
-      <div class="campus-col">
-        <img src="https://m.media-amazon.com/images/M/MV5BMDkyMmZlN2EtMzhlNC00ODc5LTk0ODgtOWRlNzRkYzRkNTdmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg">
-        <div class="layer">
-          <h3>Mission Impossible</h3>
-        </div>
-      </div>
-
+    <div class="container">
       <div class="row">
-        <div class="campus-col">
-          <img src="https://m.media-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_FMjpg_UX1000_.jpg">
-          <div class="layer">
-            <h3>Incidious</h3>
+        <div class="col-lg-3 col-sm-4">
+          <div class="campus-col">
+            <img src="https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" class="img-fluid">
+            <div class="layer">
+              <h3>Little Mermaid</h3>
+            </div>
           </div>
         </div>
-        <div class="campus-col">
-          <img src="https://upload.wikimedia.org/wikipedia/en/b/b4/Spider-Man-_Across_the_Spider-Verse_poster.jpg">
-          <div class="layer">
-            <h3>Spider Man</h3>
+        <div class="col-lg-3 col-sm-4">
+          <div class="campus-col">
+            <img src="https://m.media-amazon.com/images/M/MV5BZjYxYWVjMDMtZGRjZS00ZDE4LTk0OWUtMjUyOTI4MmYxNjgwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SL1024_.jpg" class="img-fluid">
+            <div class="layer">
+              <h3>Elemental</h3>
+            </div>
           </div>
         </div>
-        <div class="campus-col">
-          <img src="https://i.ytimg.com/vi/45cv1sNf_fo/movieposter_en.jpg">
-          <div class="layer">
-            <h3>Flash</h3>
+        <div class="col-lg-3 col-sm-4">
+          <div class="campus-col">
+            <img src="https://m.media-amazon.com/images/M/MV5BZTNiNDA4NmMtNTExNi00YmViLWJkMDAtMDAxNmRjY2I2NDVjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg" class="img-fluid">
+            <div class="layer">
+              <h3>Transformers</h3>
+            </div>
           </div>
         </div>
-        <div class="campus-col">
-          <img src="https://m.media-amazon.com/images/M/MV5BZTNiNDA4NmMtNTExNi00YmViLWJkMDAtMDAxNmRjY2I2NDVjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg">
-          <div class="layer">
-            <h3>Transformers</h3>
-          </div>
-        </div>
-      </div>
-
-      <!-- Coming Soon Card-->
-      <h2 class="pt-5 pb-5 fw-bold text-start">Coming Soon</h2>
-
-      <div class="row">
-        <div class="campus-col">
-          <img src="https://hips.hearstapps.com/hmg-prod/images/best-teen-movies-2023-magic-flute-640755619b5a5.jpg">
-          <div class="layer">
-            <h3>The Magic Flute</h3>
-          </div>
-        </div>
-        <div class="campus-col">
-          <img src="https://cdn.moviefone.com/admin-uploads/posters/meg2-movie-poster_1683589009.jpg?d=360x540&q=60">
-          <div class="layer">
-            <h3>Meg2</h3>
-          </div>
-        </div>
-        <div class="campus-col">
-          <img src="https://lumiere-a.akamaihd.net/v1/images/au_poster_marvel_antmanquantumaniapayoff_4067ec05.jpeg">
-          <div class="layer">
-            <h3>Quantumania</h3>
-          </div>
-        </div>
-        <div class="campus-col">
-          <img src="https://cdn.moviefone.com/admin-uploads/posters/kraventhehunter-movie-poster_1687208160.jpg?d=360x540&q=60">
-          <div class="layer">
-            <h3>Krawen</h3>
+        <div class="col-lg-3 col-sm-4">
+          <div class="campus-col">
+            <img src="https://m.media-amazon.com/images/M/MV5BMDkyMmZlN2EtMzhlNC00ODc5LTk0ODgtOWRlNzRkYzRkNTdmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" class="img-fluid">
+            <div class="layer">
+              <h3>Mission Impossible</h3>
+            </div>
           </div>
         </div>
 
-        <div class="row">
+        <div class="col-lg-3 col-sm-4">
           <div class="campus-col">
-            <img src="https://chstarmacdotcom.files.wordpress.com/2021/01/img_1441.jpg">
+            <img src="https://m.media-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_FMjpg_UX1000_.jpg" class="img-fluid">
             <div class="layer">
-              <h3>Jumgle Cruise</h3>
+              <h3>Incidious</h3>
             </div>
           </div>
+        </div>
+        <div class="col-lg-3 col-sm-4">
           <div class="campus-col">
-            <img src="https://www.nation.com.pk/print_images/large/2022-07-05/three-movies-to-hit-cinema-screens-on-eid-ul-azha-1662029333-8449.jpg">
+            <img src="https://upload.wikimedia.org/wikipedia/en/b/b4/Spider-Man-_Across_the_Spider-Verse_poster.jpg" class="img-fluid">
             <div class="layer">
-              <h3>London</h3>
+              <h3>Spider Man</h3>
             </div>
           </div>
+        </div>
+        <div class="col-lg-3 col-sm-4">
           <div class="campus-col">
-            <img src="https://www.digitaltrends.com/wp-content/uploads/2023/03/liLN69YgoovHVgmlHJ876PKi5Yi.jpg?p=1#038;p=1.jpg">
+            <img src="https://i.ytimg.com/vi/45cv1sNf_fo/movieposter_en.jpg" class="img-fluid">
             <div class="layer">
-              <h3>Ghost</h3>
+              <h3>Flash</h3>
             </div>
           </div>
+        </div>
+        <div class="col-lg-3 col-sm-4">
           <div class="campus-col">
-            <img src="https://hips.hearstapps.com/hmg-prod/images/disney-movies-2023-guardians-of-the-galaxy-1673546742.jpeg">
+            <img src="https://m.media-amazon.com/images/M/MV5BZTNiNDA4NmMtNTExNi00YmViLWJkMDAtMDAxNmRjY2I2NDVjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg" class="img-fluid">
             <div class="layer">
-              <h3>Guardians Galaxy</h3>
+              <h3>Transformers</h3>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+
+  <!-- Coming Soon Card-->
+  <div class="container">
+    <h2 class="pt-5 pb-5 fw-bold text-start">Coming Soon</h2>
+    <div class="row">
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/best-teen-movies-2023-magic-flute-640755619b5a5.jpg" class="img-fluid">
+          <div class="layer">
+            <h3>The Magic Flute</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://cdn.moviefone.com/admin-uploads/posters/meg2-movie-poster_1683589009.jpg?d=360x540&q=60" class="img-fluid">
+          <div class="layer">
+            <h3>Meg2</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://lumiere-a.akamaihd.net/v1/images/au_poster_marvel_antmanquantumaniapayoff_4067ec05.jpeg" class="img-fluid">
+          <div class="layer">
+            <h3>Quantumania</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://cdn.moviefone.com/admin-uploads/posters/kraventhehunter-movie-poster_1687208160.jpg?d=360x540&q=60" class="img-fluid">
+          <div class="layer">
+            <h3>Krawen</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://chstarmacdotcom.files.wordpress.com/2021/01/img_1441.jpg" class="img-fluid">
+          <div class="layer">
+            <h3>Jumgle Cruise</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://www.nation.com.pk/print_images/large/2022-07-05/three-movies-to-hit-cinema-screens-on-eid-ul-azha-1662029333-8449.jpg" class="img-fluid">
+          <div class="layer">
+            <h3>London</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://www.digitaltrends.com/wp-content/uploads/2023/03/liLN69YgoovHVgmlHJ876PKi5Yi.jpg?p=1#038;p=1.jpg" class="img-fluid">
+          <div class="layer">
+            <h3>Ghost</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-4">
+        <div class="campus-col">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/disney-movies-2023-guardians-of-the-galaxy-1673546742.jpeg" class="img-fluid">
+          <div class="layer">
+            <h3>Guardians Galaxy</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <?php include 'footer.php'; ?>
-
-
-  <!-- End card -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
-
